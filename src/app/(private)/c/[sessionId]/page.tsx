6a323f394075ff6564dc447d4818c7ed466d13e1
Fatwa Linovera <fatwalinovera@gmail.com>
2026-01-21 14:58:16 +0700

    styling

--- src/app/(private)/c/[sessionId]/page.tsx
+++ src/app/(private)/c/[sessionId]/page.tsx
@@ -20,7 +20,7 @@ import useDataState from "@/hooks/useDataState";
 import { useScrollBottom } from "@/hooks/useScrollBottom";
 import { isEmptyArray } from "@/utils/array";
 import { formatDate } from "@/utils/formatter";
-import { HStack } from "@chakra-ui/react";
+import { Badge, HStack } from "@chakra-ui/react";
 import { ArrowDownIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useRef } from "react";
@@ -61,7 +61,7 @@ export default function Page() {
     empty: <FeedbackNoData />,
     notFound: <FeedbackNotFound />,
     loaded: (
-      <CContainer flex={1} gap={4}>
+      <CContainer flex={1} gap={4} px={2}>
         {activeChat.messages.map((message: Interface__ChatMessage) => {
           if (message.role === "user") {
             return (
@@ -79,6 +79,12 @@ export default function Page() {
             return (
               <CContainer key={message.id} gap={2}>
                 <MarkdownChat>{message.content}</MarkdownChat>
+
+                <HStack>
+                  {message?.sources?.map((source, index) => {
+                    return <Badge key={index}>{source}</Badge>;
+                  })}
+                </HStack>
 
                 <HStack wrap={"wrap"}>
                   <Clipboard>{message.content}</Clipboard>
